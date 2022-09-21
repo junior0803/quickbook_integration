17d1851315973fb0466bfae240f6afe349f324bf
usan <cusagchapda@gmail.com>
2022-09-22 07:40:39 +0900

    line update

--- quickbooks-github/routes/routes.js
+++ quickbooks-github/routes/routes.js
@@ -63,15 +63,21 @@ router.post('/create_invoice', function (req, res) {
                 console.log("Line : ")
                 console.log(respJson['QueryResponse']['Invoice'][0]['Line'])
                 var Line = respJson['QueryResponse']['Invoice'][0]['Line']
+                var newLineDef = false
                 Line.forEach((line, index) => {
                     console.log(line)
                     if (line["SalesItemLineDetail"] !== undefined) {                        
                         if (line["SalesItemLineDetail"]["ItemRef"]["name"] === ItemRef[0]) {
                             Line[index]["Amount"] = invoice_amt
                             Line[index]["SalesItemLineDetail"] = {"ItemRef": {"value": ItemRef[1],"name": ItemRef[0]},"Qty": invoice_qty}
+                            newLineDef = true
                         }
                     }
                 })
+                if (newLineDef) {
+                    Line.push({"Amount": invoice_amt, "DetailType": "SalesItemLineDetail", "SalesItemLineDetail": {"ItemRef": {"value": ItemRef[1], "name": ItemRef[0]},"Qty": invoice_qty}})    
+                }
+                
                 respJson['QueryResponse']['Invoice'][0]['Line'] = Line
                 qbo.updateInvoice(
                     respJson['QueryResponse']['Invoice'][0], function (error, invoice) 
